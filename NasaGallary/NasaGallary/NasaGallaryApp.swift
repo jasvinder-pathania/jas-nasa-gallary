21ab721c84be00ba46284589ac33cf49aee0a791
Jasvinder Singh <58765087+jasvinder-pathania@users.noreply.github.com>
2022-09-18 11:18:15 +0530

    project initialization

---
+++ NasaGallary/NasaGallary/NasaGallaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  NasaGallaryApp.swift
+//  NasaGallary
+//
+//  Created by Jasvinder Singh on 18/09/22.
+//
+
+import SwiftUI
+
+@main
+struct NasaGallaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
